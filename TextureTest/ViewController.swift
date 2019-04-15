f0ab77bc78adfdd7f11275e3cd02bab49b06277b
meccatol1 <hyungsung.kim@navercorp.com>
2019-04-15 22:48:16 +0900

    Initial Commit

---
+++ TextureTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextureTest
+//
+//  Created by Hyungsung Kim on 15/04/2019.
+//  Copyright © 2019 cream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TextureTestTests/TextureTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  TextureTestTests.swift
+//  TextureTestTests
+//
+//  Created by Hyungsung Kim on 15/04/2019.
+//  Copyright © 2019 cream. All rights reserved.
+//
+
+import XCTest
+@testable import TextureTest
+
+class TextureTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
